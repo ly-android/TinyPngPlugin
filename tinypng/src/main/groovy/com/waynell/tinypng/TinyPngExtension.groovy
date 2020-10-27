3d5f899a17ed23fb40628d5ef3d96fc5b5a44f48
liyong1 <liyong1@yy.com>
2020-10-27 20:02:56 +0800

    first create

---
+++ tinypng/src/main/groovy/com/waynell/tinypng/TinyPngExtension.groovy
@@ -0,0 +1,29 @@
+package com.waynell.tinypng
+
+/**
+ * Create On 27/10/2020
+ * @author liyong1
+ */
+class TinyPngExtension {
+    ArrayList<String> apiKeys
+    ArrayList<String> whiteList
+    ArrayList<String> resourceDir
+    ArrayList<String> resourcePattern
+
+    TinyPngExtension() {
+        apiKeys = []
+        whiteList = []
+        resourceDir = []
+        resourcePattern = []
+    }
+
+    @Override
+    String toString() {
+        return "TinyPngExtension{" +
+                "apiKey='" + apiKeys + '\'' +
+                ", whiteList=" + whiteList +
+                ", resourceDir=" + resourceDir +
+                ", resourcePattern=" + resourcePattern +
+                '}';
+    }
+}
