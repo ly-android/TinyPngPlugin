98209132dbe39d351907ab00d7030ef59e7a230b
liyong1 <liyong1@yy.com>
2020-10-27 23:26:35 +0800

    update compress logic

---
+++ buildSrc/src/main/groovy/com/allen/tinypng/TinyPngExtension.groovy
@@ -0,0 +1,32 @@
+package com.allen.tinypng
+
+/**
+ * Create On 27/10/2020
+ * @author liyong1
+ */
+class TinyPngExtension {
+    ArrayList<String> apiKeys
+    ArrayList<String> whiteList
+    ArrayList<String> resourceDir
+    ArrayList<String> resourcePattern
+    boolean enable
+
+    TinyPngExtension() {
+        apiKeys = []
+        whiteList = []
+        resourceDir = []
+        resourcePattern = []
+        enable = true
+    }
+
+    @Override
+    String toString() {
+        return "TinyPngExtension{" +
+                "apiKeys='" + apiKeys + '\'' +
+                ", whiteList=" + whiteList +
+                ", resourceDir=" + resourceDir +
+                ", enable=" + enable +
+                ", resourcePattern=" + resourcePattern +
+                '}';
+    }
+}
